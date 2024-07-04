cf57b5b093d9b24eddb51e0de6f2ea75404057fe
sinedied <noda@free.fr>
2024-07-04 18:06:57 +0200

    feat: add langchain.js example

---
+++ samples/langchain.js
@@ -0,0 +1,43 @@
+import { AzureChatOpenAI, AzureOpenAI, AzureOpenAIEmbeddings } from '@langchain/openai';
+
+// Chat completion
+const model = new AzureChatOpenAI({
+  // This is where you point to your local server
+  azureOpenAIBasePath: 'http://localhost:4041/openai/deployments',
+
+  // Parameters below must be provided but are not used by the local server
+  azureOpenAIApiKey: '123456',
+  azureOpenAIApiVersion: '2024-02-01',
+  azureOpenAIApiDeploymentName: 'gpt-4'
+});
+
+const completion = await model.invoke([{ type: 'human', content: 'Say hello!' }]);
+console.log(completion.content);
+
+// Text completion
+const llm = new AzureOpenAI({
+  // This is where you point to your local server
+  azureOpenAIBasePath: 'http://localhost:4041/openai/deployments',
+
+  // Parameters below must be provided but are not used by the local server
+  azureOpenAIApiKey: '123456',
+  azureOpenAIApiVersion: '2024-02-01',
+  azureOpenAIApiDeploymentName: 'gpt-4'
+});
+
+const text = await llm.invoke('Say hello in French: ');
+console.log(text);
+
+// Embeddings
+const embeddings = new AzureOpenAIEmbeddings({
+  // This is where you point to your local server
+  azureOpenAIBasePath: 'http://localhost:4041/openai/deployments',
+
+  // Parameters below must be provided but are not used by the local server
+  azureOpenAIApiKey: '123456',
+  azureOpenAIApiVersion: '2024-02-01',
+  azureOpenAIApiEmbeddingsDeploymentName: 'text-embedding-ada-002'
+});
+
+const vector = await embeddings.embedQuery('Once upon a time');
+console.log(vector);
