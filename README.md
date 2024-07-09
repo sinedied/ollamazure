6257bb99aef1ef2c138a340e2106804ec37ed91f
Yohan Lasorsa <noda@free.fr>
2024-07-09 15:48:51 +0000

    docs: add semantic kernel example

--- README.md
+++ README.md
@@ -303,3 +303,30 @@ Console.WriteLine(completion.Content[0].Text);
 ```
 
 </details>
+
+
+<details>
+<summary><b>Semantic Kernel</b></summary><br>
+
+```csharp
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var builder = Kernel.CreateBuilder();
+
+// Chat completion
+builder.AddAzureOpenAIChatCompletion(
+    "gpt-4",                   // Deployment Name (not used by the local server)
+    "http://localhost:4041",   // Azure OpenAI Endpoint 
+    "123456");                 // Azure OpenAI Key (not used by the local server)
+
+var kernel = builder.Build();
+var chatFunction = kernel.CreateFunctionFromPrompt(@"{{$input}}");
+
+var chatCompletion = await kernel.InvokeAsync(chatFunction, new() { ["input"] = "Say hello!" });
+
+Console.WriteLine(chatCompletion);
+```
+
+</details>
+
---
+++ samples/csharp/semantic-kernel/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var builder = Kernel.CreateBuilder();
+
+// Chat completion
+builder.AddAzureOpenAIChatCompletion(
+    "gpt-4",                   // Deployment Name (not used by the local server)
+    "http://localhost:4041",   // Azure OpenAI Endpoint 
+    "123456");                 // Azure OpenAI Key (not used by the local server)
+
+var kernel = builder.Build();
+var chatFunction = kernel.CreateFunctionFromPrompt(@"{{$input}}");
+
+var chatCompletion = await kernel.InvokeAsync(chatFunction, new() { ["input"] = "Say hello!" });
+
+Console.WriteLine(chatCompletion);
+
+// Embeddings
+// var embeddingGenerator = new AzureOpenAITextEmbeddingGenerationService( 
+//     "gpt-4",                   // Deployment Name (not used by the local server)
+//     "http://localhost:4041",   // Azure OpenAI Endpoint 
+//     "123456");                 // Azure OpenAI Key (not used by the local server)
+
+// var embedding = await embeddingGenerator.GenerateEmbeddingsAsync(["Once upon a time", "The end."]);
+
+// Console.WriteLine(embedding);
